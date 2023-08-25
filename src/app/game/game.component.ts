47b7d0500c107af4d7870791c583131c913c2bb5
Jonas Wemmers <jonas.wemmers@freenet.de>
2023-08-25 04:37:21 +0200

    Card Animation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game = new Game(); // Hier wird game direkt initialisiert
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if(!this.pickCardAnimation){
+    this.currentCard = this.game.stack.pop() as string;
+    this.pickCardAnimation = true;
+
+    setTimeout(()=>{
+      this.pickCardAnimation = false;
+    }, 1500);
+  }
+  }  
 }
