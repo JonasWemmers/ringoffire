8d899bc398918d14c2cf8a995d69eb77cef22f01
Jonas Wemmers <jonas.wemmers@freenet.de>
2023-08-25 22:14:17 +0200

    Install Angular Material Design & build the Button to add Player

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
-export class PlayerComponent {
+export class PlayerComponent implements OnInit {
+
+  @Input() name!: string; // Das Ausrufezeichen ("!") zeigt TypeScript an, dass du sicher bist, dass die Eigenschaft später zugewiesen wird.
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
 
 }
